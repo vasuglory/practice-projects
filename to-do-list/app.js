29d2d4b343c57c07e1285bcfec94acef1518fda0
vasuglory <nallagorlavasantha@gmail.com>
2025-12-15 00:10:08 +0530

    3. to-do-list deleting and adding task is complete.

--- to-do-list/app.js
+++ to-do-list/app.js
@@ -1,17 +1,88 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
+import "dotenv/config";
 const port = 2000;
 const app = express();
+
 app.set("view engine","ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.json());
+
+const db = new pg.Pool({
+  user:process.env.user,
+  password:process.env.password,
+  port:process.env.port,
+  host:process.env.host,
+  database:process.env.database
+});
+
+
+//render an structure of page quickly
 app.get("/",(_req,res)=>{
   //Read the data from tasks table and render it to ejs file
   res.render("home");
-})
-app.post("/add",(req,res)=>{
-  console.log(req.body.task);
+});
+
+//get all tasks from the database
+app.get("/getAllTasks",async (_req,res)=>{
+  try{
+    let tasks = [];
+    const result = await db.query("SELECT * FROM tasks WHERE is_task_done = false ORDER BY id ASC");
+    if(result.rowCount > 0){
+      //has some tasks that isn't completed
+      result.rows.forEach(row =>{
+        tasks.push(row);
+      });
+      console.log(JSON.stringify(tasks[0].created_on));
+      
+    }
+    return res.json(tasks);
+  }catch(err){
+    console.log("Error while getting all tasks from db: "+err.stack);
+  }
+  
+});
+
+//add a task to the database
+app.post("/add",async (req,res)=>{
+  let task = req.body.task;
+  let newTask = {
+    
+  }
+  if(task){
+    try{
+      const result = await db.query("INSERT INTO tasks(task) VALUES($1) RETURNING id",[task]);
+      console.log(result.rows[0].id);
+      newTask.id = result.rows[0].id;
+      newTask.task = task;
+    }
+    catch(err){
+      console.log("error while adding a task to db: "+err.stack);
+      return res.status(500).send("database error");
+    }
+    }
+    console.log(newTask.task)
+    return res.json(newTask);
+    
+  
+});
+
+app.patch("/deleteTask",async (req,res)=>{
+  let id = req.body.id;
+  let status = {
+    is_task_done:false
+  }
+  if(id){
+    try{
+      const result = await db.query("UPDATE tasks SET is_task_done = TRUE WHERE id = $1 RETURNING is_task_done",[id]);
+      status.is_task_done = result.rows[0].is_task_done;
+    }catch(err){
+      console.log("error while updating task status: "+err.stack);
+    }
+  }
+  return res.json(status);
 })
 app.listen(port,()=>{
   console.log(`server started on port: ${port}`);
--- to-do-list/public/script.js
+++ to-do-list/public/script.js
@@ -1,20 +1,95 @@
-
 const form = document.getElementById("add-a-task-form");
-form.addEventListener("submit",addTask);
+form.addEventListener("submit", addTask);
 
-//when add the task button got clicked
-async function addTask(e){
-  e.preventDefault();
-  console.log(e.target.task.value);
-  const data = {
-    task: e.target.task.value
-  }
-  const result = await fetch("/add",{
-    method: "POST",
-    headers:{
-      "content-type":"application/json"
-    },
-    body:JSON.stringify(data)
-  });
+const tasksContainer = document.querySelector(".tasks-container");
+//get all tasks
+
+function tasksComponent(id, task) {
+  const taskContainer = document.createElement("div");
+  taskContainer.classList.add("task-container");
+
+  const checkboxEl = document.createElement("span");
+  checkboxEl.classList.add("round-shaped-checkbox");
+
+  const taskEl = document.createElement("p");
+  taskEl.textContent = task;
+  taskEl.classList.add("task-data");
+
+  const editIconEl = document.createElement("button");
+  editIconEl.classList.add("edit-task");
+
+  const deleteIconEl = document.createElement("button");
+  deleteIconEl.classList.add("delete-task");
+  deleteIconEl.setAttribute("task-id", id);
+  deleteIconEl.addEventListener("click",deleteTask);
+  
+  taskContainer.append(checkboxEl,taskEl,editIconEl,deleteIconEl);
+  tasksContainer.append(taskContainer);
   
 }
+
+async function addTasksToPage() {
+  const result = await fetch("/getAllTasks");
+  if (!result.ok) {
+    console.log("internal server error.");
+  } else {
+    //no server error
+    let tasks = await result.json();
+    console.log(tasks.length);
+    if (tasks.length > 0) {
+      tasks.forEach(task => {
+        tasksComponent(task.id,task.task);
+      })
+    }
+  }
+}
+addTasksToPage();
+
+//when add the task button got clicked
+async function addTask(e) {
+  e.preventDefault();
+  const data = {
+    task: e.target.task.value,
+  };
+  const result = await fetch("/add", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  if (!result.ok) {
+    console.log("try again.");
+    let msg = await result.text();
+    console.log(msg);
+  }
+  else{
+   const newTask =  await result.json();
+   if(Object.keys(newTask).length >= 1){
+    console.log("i am here");
+    console.log(newTask);
+    console.log(newTask.task)
+    tasksComponent(newTask.id,newTask.task);
+   }
+   
+  }
+}
+//delete task from home page
+async function deleteTask(){
+  let obj = {
+    id:this.getAttribute("task-id")
+  }
+  const result = await fetch("/deleteTask",{
+    method:"PATCH",
+    headers:{
+     "content-type": "application/json"
+    },
+    body:JSON.stringify(obj)
+  });
+  const status = await result.json();
+  console.log(status);
+  this.parentElement.remove();
+}
+
+
+
